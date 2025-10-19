024bc8a71cf3a4e65b139362bdf60711e145895e
AGSQ <67051233+AGSQ11@users.noreply.github.com>
2025-10-19 22:12:20 +0300

    skin upd

--- src/themes/apollo/postcss.config.js
+++ src/themes/apollo/postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    tailwindcss: { config: './src/themes/apollo/tailwind.config.js' },
+    '@tailwindcss/postcss': { config: './src/themes/apollo/tailwind.config.js' },
     autoprefixer: {},
   },
 }
--- src/themes/apollo/webpack.config.js
+++ src/themes/apollo/webpack.config.js
@@ -9,23 +9,14 @@ Encore
   .setPublicPath('/themes/apollo/build')
 
   .addEntry('apollo', './assets/js/apollo.js')
-  .addStyleEntry('theme', './assets/scss/apollo.scss')
+  .addStyleEntry('theme', './assets/css/apollo.css')
 
   .disableSingleRuntimeChunk()
   .cleanupOutputBeforeBuild()
   .enableSourceMaps(!Encore.isProduction())
   .enableVersioning(Encore.isProduction())
   
-  .enablePostCssLoader((options) => {
-      options.postcssOptions = {
-          config: './src/themes/apollo/postcss.config.js'
-      }
-  })
-  .enableSassLoader((options) => {
-      options.sassOptions = {
-          includePaths: ['../../../../node_modules']
-      }
-  })
+  .enablePostCssLoader()
 ;
 
 module.exports = Encore.getWebpackConfig();
